573c8fb2fa1e7f7e81102b00f904f56a01fb74aa
u4268987956-netizen <u4268987956@gmail.com>
2025-12-30 01:09:14 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+function addToCart(btn) {
+  let product = btn.parentElement;
+  let name = product.dataset.name;
+  let price = Number(product.dataset.price);
+
+  cart.push({ name, price });
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("به سبد اضافه شد");
+}
+
+function loadCart() {
+  let container = document.getElementById("cart-items");
+  let total = 0;
+
+  cart.forEach(item => {
+    total += item.price;
+    container.innerHTML += `
+      <div class="cart-item">
+        <span>${item.name}</span>
+        <span>${item.price.toLocaleString()} تومان</span>
+      </div>
+    `;
+  });
+
+  document.getElementById("total").innerText =
+    "جمع کل: " + total.toLocaleString() + " تومان";
+}
+
+if (document.getElementById("cart-items")) {
+  loadCart();
+}
